86d81f89c9c90822d43ab94d4039bc55f780a23b
olbol-sabai <olliewelsh123@gmail.com>
2021-02-20 13:15:55 +0000

    removed django secret key

--- reactdjangoboilerplate/reactdjangoboilerplate/settings.py
+++ reactdjangoboilerplate/reactdjangoboilerplate/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uf#r%*h^6^5$p_znko@m25&(l1vfka1hi_ko)cc225mpv!p)cy'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
